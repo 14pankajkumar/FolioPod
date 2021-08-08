995d9916f4ef920e79c9ee681a8667b1a1aa89fb
Sunil Kumar <neil001002@gmail.com>
2021-08-08 21:26:15 +0530

    nfts added, final

--- App.tsx
+++ App.tsx
@@ -1,28 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
-import InitialRoute from './screens/InitialRoute';
-import Token_Balances from './screens/Token_Balances';
+import InitialRoute from "./screens/InitialRoute";
+import Token_Balances from "./screens/Token_Balances";
+import NFTs from "./screens/NFTs";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
-      <StatusBar style="auto" />
-      <Stack.Navigator
-      initialRouteName={"MainLayout"}
-      >
-        <Stack.Screen name="MainLayout" component={InitialRoute} options={{headerShown: false}} />
-        <Stack.Screen name="Token_Balances" component={Token_Balances} options={{headerShown: false}} />
-        {/* <Stack.Screen name="Home" component={Home}  /> */}
-        {/* <Stack.Screen name="Ethereum" component={Ethereum}  /> */}
-
+      <StatusBar hidden />
+      {/* <StatusBar style="auto" /> */}
+      <Stack.Navigator initialRouteName={"MainLayout"}>
+        <Stack.Screen
+          name="MainLayout"
+          component={InitialRoute}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Token_Balances"
+          component={Token_Balances}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NFTs"
+          component={NFTs}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
--- screens/Token_Balances.tsx
+++ screens/Token_Balances.tsx
@@ -1,160 +1,264 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import { ActivityIndicator, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const Token_Balances = ({route}) => {
+import { AntDesign } from "@expo/vector-icons";
 
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([])
+const Token_Balances = ({ route }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [network, setNetwork] = useState("");
+  const [assets, setAssets] = useState("");
 
-    // useEffect(() => {
-    //     fetch(`https://stg-api.unmarshal.io/v1/ethereum/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`)
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             console.log("Response data", data);
-                
-    //         })
-            
-    // }, [] );
+  const address = `${route.params.data}`;
 
-    const getCoins = async () => {
-        try {
-            const response = await fetch(`https://stg-api.unmarshal.io/v1/ethereum/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`);
-            const res = await response.json();
-            setData(res);
-            console.log("response ", res)
-        } catch (error) {
-            console.log("there was an problem", error)
-        } finally {
-            setLoading(false);
-        }
+  const navigation = useNavigation();
+
+  // useEffect(() => {
+  //     fetch(`https://stg-api.unmarshal.io/v1/ethereum/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`)
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //             console.log("Response data", data);
+
+  //         })
+
+  // }, [] );
+
+  const ethereum = async () => {
+    try {
+      const response = await fetch(
+        `https://stg-api.unmarshal.io/v1/ethereum/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`
+      );
+      const res = await response.json();
+      setData(res);
+      console.log("response ", res);
+    } catch (error) {
+      console.log("there was an problem", error);
+    } finally {
+      setLoading(false);
     }
 
-    useEffect(() => {
-        getCoins();
-    }, [])
+    setNetwork("Ethereum");
+    setAssets("Tokens");
+  };
 
-    const bsc = async () => {
-        try {
-            const response = await fetch(`https://stg-api.unmarshal.io/v1/bsc/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`);
-            const res = await response.json();
-            setData(res);
-            console.log("response ", res)
-        } catch (error) {
-            console.log("there was an problem", error)
-        } finally {
-            setLoading(false);
-        }
+  useEffect(() => {
+    ethereum();
+  }, []);
+
+  const bsc = async () => {
+    try {
+      const response = await fetch(
+        `https://stg-api.unmarshal.io/v1/bsc/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`
+      );
+      const res = await response.json();
+      setData(res);
+      console.log("response ", res);
+    } catch (error) {
+      console.log("there was an problem", error);
+    } finally {
+      setLoading(false);
     }
 
-    const polygon = async () => {
-        try {
-            const response = await fetch(`https://stg-api.unmarshal.io/v1/matic/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`);
-            const res = await response.json();
-            setData(res);
-            console.log("response ", res)
-        } catch (error) {
-            console.log("there was an problem", error)
-        } finally {
-            setLoading(false);
-        }
+    setNetwork("BSC");
+    // setAssets("Tokens");
+  };
+
+  const polygon = async () => {
+    try {
+      const response = await fetch(
+        `https://stg-api.unmarshal.io/v1/matic/address/${route.params.data}/assets?auth_key=VGVtcEtleQ%3D%3D`
+      );
+      const res = await response.json();
+      setData(res);
+      console.log("response ", res);
+    } catch (error) {
+      console.log("there was an problem", error);
+    } finally {
+      setLoading(false);
     }
 
-    return (
-        <View style={{backgroundColor: "white", marginBottom: 10}}>
+    setNetwork("Polygon");
+    // setAssets("Tokens");
+  };
 
-            <View style={{flexDirection: "row", justifyContent: "space-between", marginTop: 20, marginHorizontal: 50, marginBottom: 10}}>
+  //   const nfts = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `https://stg-api.unmarshal.io/v1/bsc/address/${route.params.data}/nft-assets?auth_key=VGVtcEtleQ==`
+  //       );
+  //       const res = await response.json();
+  //       setNftData(res);
+  //       console.log("response ", res);
+  //     } catch (error) {
+  //       console.log("there was an problem", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
 
-                <TouchableOpacity onPress={() => getCoins()} style={styles.ethTopButton1}>
-                    <Text style={{color: "white", fontWeight: "bold"}}>Ethereum</Text>
-                </TouchableOpacity>
+  //     return (
+  //       <View style={{ flex: 1 }}>
+  //         <Text>All your NFTs</Text>
+  //       </View>
+  //     );
+  //   };
 
-                <TouchableOpacity onPress={() => bsc()} style={styles.ethTopButton}>
-                    <Text style={{color: "gray", fontWeight: "bold"}}>BSC</Text>
-                </TouchableOpacity>
-                
-                <TouchableOpacity onPress={() => polygon()} style={styles.ethTopButton}>
-                    <Text style={{color: "gray", fontWeight: "bold"}}>Polygon</Text>
-                </TouchableOpacity>
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ backgroundColor: "white", marginBottom: 10 }}>
+        <View style={styles.topButtonContainer}>
+          <TouchableOpacity
+            onPress={() => ethereum()}
+            style={styles.ethTopButton}
+          >
+            <Text style={styles.buttonText}>Ethereum</Text>
+          </TouchableOpacity>
 
-            </View>
-            {/* <Text>All your Ethereum tokens</Text> */}
-            {isLoading ? <ActivityIndicator/> : (
-            <FlatList style={{backgroundColor: "#EEEEEE", borderRadius: 10}}
-                data={data}
-                keyExtractor={({ id }, index) => id}
-                renderItem={({ item }) => (
-                    <View style={styles.coinContainer}>
-                        
-                            <View style={{flexDirection: "row", alignItems: 'center'}}>
+          <TouchableOpacity onPress={() => bsc()} style={styles.ethTopButton}>
+            <Text style={styles.buttonText}>BSC</Text>
+          </TouchableOpacity>
 
-                                <View>
-                                    <Image source={{uri: item.logo_url}} style={{height: 35, width: 35 }} />
-                                </View>
-
-                                <View style={{marginLeft: 10}} >
-                                    <Text >{item.contract_name}</Text>
-                            
-                                    <View style={{flexDirection: "row"}}>
-                                        <Text style={{color: "gray"}}>{item.contract_ticker_symbol} </Text>
-                                        <Text style={{color: "gray"}}>{(item.quote/item.quote_rate).toFixed(4)} </Text>
-                                        <Text style={{color: "gray"}}>${item.quote_rate}</Text>
-                                    </View>    
-                                </View>
-
-                            </View>
-
-                            <View>
-                                <Text>${item.quote.toFixed(4)}</Text>
-                            </View>
-                    
-                    </View>
-                )}
-                
-            />
-            )}
+          <TouchableOpacity
+            onPress={() => polygon()}
+            style={styles.ethTopButton}
+          >
+            <Text style={styles.buttonText}>Polygon</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
+        <View
+          style={{ marginLeft: 10, marginBottom: 10, flexDirection: "row" }}
+        >
+          <Text>Your all</Text>
+          <Text style={{ fontWeight: "bold" }}> {assets} </Text>
+          <Text>on</Text>
+          <Text style={{ fontWeight: "bold" }}> {network} </Text>
+          <Text>chain</Text>
+        </View>
 
-export default Token_Balances
+        {isLoading ? (
+          // <ActivityIndicator />
+          <Text style={{ textAlign: "center", justifyContent: "center" }}>
+            Loading...
+          </Text>
+        ) : (
+          <FlatList
+            style={{ borderRadius: 10 }}
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={styles.coinContainer}>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <View>
+                    <Image
+                      source={{ uri: item.logo_url }}
+                      style={{ height: 35, width: 35 }}
+                    />
+                  </View>
+
+                  <View style={{ marginLeft: 10 }}>
+                    <Text>{item.contract_name}</Text>
+
+                    <View style={{ flexDirection: "row" }}>
+                      <Text style={{ color: "gray" }}>
+                        {item.contract_ticker_symbol}{" "}
+                      </Text>
+                      <Text style={{ color: "gray" }}>
+                        {(item.quote / item.quote_rate).toFixed(4)}{" "}
+                      </Text>
+                      <Text style={{ color: "gray" }}>${item.quote_rate}</Text>
+                    </View>
+                  </View>
+                </View>
+
+                <View>
+                  <Text>${item.quote.toFixed(4)}</Text>
+                </View>
+              </View>
+            )}
+          />
+        )}
+      </View>
+      <View style={styles.bottomButtonContainer}>
+        <TouchableOpacity style={styles.ethBottomButton}>
+          <Text style={styles.buttonText}>Tokens</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NFTs", { add: address })}
+          style={styles.ethBottomButton}
+        >
+          <Text style={styles.buttonText}>NFTs</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("MainLayout")}>
+          <AntDesign name="leftcircle" size={40} color="#28FFBF" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Token_Balances;
 
 const styles = StyleSheet.create({
-    coinContainer: {
-        flexDirection: "row", 
-        alignItems: "center", 
-        justifyContent: "space-between", 
-        // paddingVertical: 5, 
-        marginHorizontal: 10,
-        borderBottomWidth: 0.5,
-        marginTop: 10,
-        paddingBottom: 10,
-        // marginBottom: 5
-        // elevation: 2
-    },
+  coinContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 10,
+    borderBottomWidth: 0.5,
+    marginTop: 10,
+    paddingBottom: 10,
+  },
 
-    ethTopButton1: {
-        borderWidth: 2,
-        backgroundColor: "#28FFBF",
-        height: 40,
-        width: "30%",
-        borderRadius: 50,
-        justifyContent: "center",
-        alignItems: "center",
-        // elevation: 10,
+  topButtonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+    marginHorizontal: 40,
+    marginBottom: 10,
+  },
 
-    },
+  ethTopButton: {
+    borderWidth: 2,
+    borderColor: "#28FFBF",
+    height: 40,
+    width: "30%",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    ethTopButton: {
-        borderWidth: 2,
-        borderColor: "#28FFBF",
-        // backgroundColor: "#28FFBF",
-        height: 40,
-        width: "30%",
-        borderRadius: 50,
-        justifyContent: "center",
-        alignItems: "center"
+  bottomButtonContainer: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    left: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 40,
+  },
 
-    }
-})
+  ethBottomButton: {
+    backgroundColor: "#28FFBF",
+    height: 40,
+    width: "30%",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
+  buttonText: {
+    color: "gray",
+    fontWeight: "bold",
+  },
+});
